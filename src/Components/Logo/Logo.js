c371fcd565cb1e1da0aafebb15696066351c0c44
Reinoso <jreinoso@amazon.com>
2021-02-16 21:26:24 -0500

    added baseball tilted logo

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Tilt from 'react-tilt'
 import '../Logo/logo.css'
-import PCimage from '../Logo/computerIcon.jpg'
+import PCimage from '../Logo/Baseball.png'
 
 //Since im not controlling state here i could do a function
 const Logo = () => {
     return (
 <div>
-<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 200, width: 200 }} >
- <div className="Tilt-inner"><img alt="Logo" src={PCimage}/> </div>
+<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 200, width: 200,  boxShadow: "5px 5px 5px 10px #888888", borderRadius:"20px",marginLeft:"2%"}} >
+ <div className="Tilt-inner"><img alt="Logo" src={PCimage} style={{margin:"30%", width:"40%"}}/> </div>
 </Tilt>
 </div>
     )
