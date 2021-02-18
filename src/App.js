07f2c46f3c7cad13d974e061ce38f7a18cd1dfb5
Reinoso <jreinoso@amazon.com>
2021-02-17 21:21:27 -0500

    Added particles js library to create that cool moving effect to background

--- src/App.js
+++ src/App.js
@@ -8,23 +8,29 @@ import Rank from './Components/Rank/Rank'
 import Particles from 'react-particles-js';
 
 const particlesOptions = {
-  particles: {
-    value:30,
-    density:{
-      enable:true,
-      value_area:800
-    }
-
+particles:{
+number:{
+  value:80,
+  density:{
+    enable:true,
+    value_area:800
   }
+  
+}
+
+
+}
 
 }
 
 class App extends Component {
   render(){
     return (
-      <div >
+      <div className="App">
         {/*create class and add css to prevent this from overwritting our app*/}
-        <Particles className ="particles"params={particlesOptions}/>
+        <Particles className ="particles"
+        params={particlesOptions}
+        />
         <Navigation/>
         <Logo/>
         <Rank/>
--- src/Components/Rank/Rank.js
+++ src/Components/Rank/Rank.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Rank = () => {
     return (
-        <div style={{justifyContent:"center", display:"flex", alignItems:"center,",flexDirection:"column"}}>
+        <div >
          <div className="white f3">Jose, your current Rank is..</div>
          <div className="white f1">#1</div>
         </div>
