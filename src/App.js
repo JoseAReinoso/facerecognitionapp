de5f9584bd201d1ee54ea00c85dcb5edf534524a
Reinoso <jreinoso@amazon.com>
2021-02-22 20:59:26 -0500

    switched to the face detect model from Clarifai API and switch to console log the precise location where i will put the little square to appear

--- src/App.js
+++ src/App.js
@@ -47,11 +47,11 @@ class App extends Component {
   }
   onSubmit = ()=>{
     this.setState({imageUrl:this.state.input})
-    app.models.predict(Clarifai.COLOR_MODEL,
+    app.models.predict(Clarifai.FACE_DETECT_MODEL,
     this.state.input)
     .then(
       function(response){
-     console.log(response)
+     console.log(response.outputs[0]['data']['regions'][0]["region_info"]['bounding_box'])
       },
       function(error){
      console.log("Error that im getting= ",error)
