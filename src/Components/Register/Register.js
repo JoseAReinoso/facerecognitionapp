b0b12526e75e525593ddc2ac8c5c37d77e4029b6
Reinoso <jreinoso@amazon.com>
2021-03-14 12:43:04 -0400

    register validation done to make sure users sends requested values by the backend

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -32,7 +32,7 @@ class Register extends React.Component {
     })
     .then(response => response.json())
     .then(user => {
-      if (user){
+      if (user != 'Incorrect form validation'){
        this.props.loadUser(user)  
        this.props.onRouteChange('home')
       }
